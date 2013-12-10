7ae493e646bd67c43b2de7b5e5784c8aa0487b69
Mario Casciaro <mariocasciaro@gmail.com>
2013-12-10 01:48:43 +0000

    Add progress bar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 var async = require('async'),
+  ProgressBar = require('progress'),
   microtime = require('microtime');
 
 function Benchpress(options) {
@@ -8,14 +9,6 @@ function Benchpress(options) {
   this.iterations = options.iterations || 500;
   this._resetProfiler();
 }
-
-Benchpress.prototype.log = function(message) {
-  process.stdout.write(message);
-};
-
-Benchpress.prototype.logln = function(message) {
-  console.log(message);
-};
 
 Benchpress.prototype._resetProfiler = function() {
   this.timeAccumulator = 0;
@@ -32,27 +25,34 @@ Benchpress.prototype._stopProfiler = function() {
 
 Benchpress.prototype.run = function(done) {
   var self = this;
-  self.logln("Starting suite.");
+  
+  console.log("Starting suite.");
   async.eachSeries(this.benchmarks, function(bench, done) {
-    self.log("Benchmark '" + bench.name + "': ");
-    async.nextTick(function() {
-      self.runBenchpress(bench, function(err) {
-        if(err) {
-          self.logln("There was an error: " + (err.stack || err));
-        } else {
-          self.logln(Math.round(bench.iterations / self.timeAccumulator)  + " ops/sec ("+bench.iterations+" iterations)");
-        }
-        done();
-      });
+    var bar = new ProgressBar("Running '" + bench.name + "' [:bar] :percent :etas ", {
+      complete: '=',
+      incomplete: ' ',
+      width: 20,
+      total: bench.iterations,
+      clear: true
+    });
+    bench.progress = bar;
+    self.runBenchmark(bench, function(err) {
+      if(err) {
+        console.log("There was an error: " + (err.stack || err));
+      } else {
+        console.log(bench.name + ': ' + Math.round(bench.iterations / self.timeAccumulator)  + 
+          " ops/sec ("+bench.iterations+" iterations)");
+      }
+      done();
     });
   }, function(err) {
-    self.log("Run complete");
+    console.log("Run complete");
     done(err);
   });
 };
 
 
-Benchpress.prototype.runBenchpress = function(bench, done) {
+Benchpress.prototype.runBenchmark = function(bench, done) {
   var self = this;
 
   async.series([
@@ -67,42 +67,45 @@ Benchpress.prototype.runBenchpress = function(bench, done) {
     },
     function(done){
       async.timesSeries(bench.iterations, function(n, done) {
-        async.series([
-          function(done) {
-            if(!bench.beforeEach) return done();
-            if(bench.beforeEach.length === 0) {
-              bench.beforeEach();
-              done();
-            } else {
-              bench.beforeEach(done);
-            }
-          },
-          function(done) {
-            if(!bench.fn) return done("Must provide a function to benchmark for test '" + bench.name + "'");
-            self._resetProfiler();
-            if(bench.fn.length === 0) {
-              self._startProfiler();
-              bench.fn();
-              self._stopProfiler();
-              done();
-            } else {
-              self._startProfiler();
-              bench.fn(function(err) {
+        async.setImmediate(function() {
+          async.series([
+            function(done) {
+              if(!bench.beforeEach) return done();
+              if(bench.beforeEach.length === 0) {
+                bench.beforeEach();
+                done();
+              } else {
+                bench.beforeEach(done);
+              }
+            },
+            function(done) {
+              if(!bench.fn) return done("Must provide a function to benchmark for test '" + bench.name + "'");
+              self._resetProfiler();
+              if(bench.fn.length === 0) {
+                self._startProfiler();
+                bench.fn();
                 self._stopProfiler();
-                done(err);
-              });
+                done();
+              } else {
+                self._startProfiler();
+                bench.fn(function(err) {
+                  self._stopProfiler();
+                  bench.progress.tick(1);
+                  done(err);
+                });
+              }
+            },
+            function(done) {
+              if(!bench.afterEach) return done();
+              if(bench.afterEach.length === 0) {
+                bench.afterEach();
+                done();
+              } else {
+                bench.afterEach(done);
+              }
             }
-          },
-          function(done) {
-            if(!bench.afterEach) return done();
-            if(bench.afterEach.length === 0) {
-              bench.afterEach();
-              done();
-            } else {
-              bench.afterEach(done);
-            }
-          }
-        ], done);
+          ], done);
+        });
       }, done);
     },
     function(done){
@@ -113,7 +116,7 @@ Benchpress.prototype.runBenchpress = function(bench, done) {
       } else {
         bench.afterAll(done);
       }
-    },
+    }
   ], done);
 };
 
